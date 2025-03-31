69418e32c3a56d51660117a380fc1e7fb96f0f08
marektes <marektesil@seznam.cz>
2025-03-31 11:50:08 +0200

    Přidání scriptu pro projekt

---
+++ Script pro projekt.sql
@@ -0,0 +1,38 @@
+--Primární tabulky--
+
+select *
+from czechia_payroll cp 
+
+select *
+from czechia_payroll_calculation cpc 
+
+select *
+from czechia_payroll_industry_branch cpib 
+
+select *
+from czechia_payroll_unit cpu 
+
+select *
+from czechia_payroll_value_type cpvt 
+
+select *
+from czechia_price cp 
+
+select *
+from czechia_price_category cpc 
+
+--Číselníky sdílených informací o ČR--
+
+select *
+from czechia_region cr 
+
+select *
+from czechia_district cd 
+
+--Dodatečné tabulky--
+
+select *
+from countries c 
+
+select *
+from economies e 
